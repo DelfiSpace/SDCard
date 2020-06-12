ad00f03aaefb055d0a91240e385917c0e142bc33
Casper <broekhuizen.casper@gmail.com>
2020-06-12 14:31:17 +0200

    Wrapper around lfs filesystem

--- LittleFS.cpp
+++ LittleFS.cpp
@@ -70,6 +70,7 @@ int LittleFS::mount(SDCard *bd)
     _config.prog  = lfs_sd_prog;
     _config.erase = lfs_sd_erase;
     _config.sync  = lfs_sd_sync;
+
     _config.read_size   = bd->get_read_size();
     if (_config.read_size < _read_size) {
         _config.read_size = _read_size;
@@ -82,25 +83,49 @@ int LittleFS::mount(SDCard *bd)
     if (_config.block_size < _block_size) {
         _config.block_size = _block_size;
     }
+    _config.lookahead_size = _lookahead;
     _config.block_count = bd->size() / _config.block_size;
-//    _config.lookahead = 32 * ((_config.block_count + 31) / 32);
-//    if (_config.lookahead > _lookahead) {
-//        _config.lookahead = _lookahead;
-//    }
+
+    // block device configuration
+    _config.cache_size = LFS_CACHE_SIZE*_config.read_size;
+    _config.block_cycles = LFS_BLOCKCYCLES;
+
     _config.read_buffer = readBuf;
     _config.prog_buffer = progBuf;
-    _config.lookahead_buffer = lookaheadBuf;
-//    _config.file_buffer = &fileBuf;
+    _config.lookahead_buffer = lkahBuf;
+
+    //Initialize with 0, to avoid some random value sitting there.
+    _config.name_max = 0;
+    _config.file_max = 0;
+    _config.attr_max = 0;
 
     err = lfs_mount(&_lfs, &_config);
     if (err) {
         _bd = NULL;
-        return err;
+        return (err);
     }
 
     return 0;
 }
 
+int LittleFS::unmount()
+{
+    int res = 0;
+    if (_bd) {
+        int err = lfs_unmount(&_lfs);  //releases resources..
+        if (err && !res) {
+            res = (err);
+        }
+
+        err = _bd->deinit(); //does actually nothing, deinit an sd?
+        if (err && !res) {
+            res = err;
+        }
+
+        _bd = 0;
+    }
+    return res;
+}
 
 int LittleFS::format(lfs_size_t read_size, lfs_size_t prog_size,
                              lfs_size_t block_size, lfs_size_t lookahead)
@@ -129,15 +154,21 @@ int LittleFS::format(lfs_size_t read_size, lfs_size_t prog_size,
     if (_config.block_size < _block_size) {
         _config.block_size = _block_size;
     }
+    _config.lookahead_size = lookahead;
     _config.block_count = _bd->size() / _config.block_size;
-//    _config.lookahead = 32 * ((_config.block_count + 31) / 32);
-//    if (_config.lookahead > _lookahead) {
-//        _config.lookahead = _lookahead;
-//    }
+
+    // block device configuration
+    _config.cache_size = LFS_CACHE_SIZE*_config.read_size;
+    _config.block_cycles = LFS_BLOCKCYCLES;
+
     _config.read_buffer = readBuf;
     _config.prog_buffer = progBuf;
-    _config.lookahead_buffer = lookaheadBuf;
-//    _config.file_buffer = &fileBuf;
+    _config.lookahead_buffer = lkahBuf;
+
+    //Initialize with 0, to avoid some random value sitting there.
+    _config.name_max = 0;
+    _config.file_max = 0;
+    _config.attr_max = 0;
 
     err = lfs_format(&_lfs, &_config);
     if (err) {
@@ -150,4 +181,138 @@ int LittleFS::format(lfs_size_t read_size, lfs_size_t prog_size,
     }
 
     return 0;
+}
+
+int LittleFS::remove(const char *filename)
+{
+    int err = lfs_remove(&_lfs, filename);
+    return (err);
+}
+
+int LittleFS::rename(const char *oldname, const char *newname)
+{
+    int err = lfs_rename(&_lfs, oldname, newname);
+    return (err);
+}
+
+int LittleFS::stat(const char *name, lfs_info *info)
+{
+    int err = lfs_stat(&_lfs, name, info);
+    return (err);
+}
+
+int LittleFS::mkdir(const char *name)
+{
+    int err = lfs_mkdir(&_lfs, name);
+    return (err);
+}
+
+int LittleFS::statvfs(const char *name, statvfs_t *st)
+{
+    memset(st, 0, sizeof(struct statvfs));
+
+    lfs_ssize_t in_use = 0;
+    in_use = lfs_fs_size(&_lfs);
+    if (in_use < 0) {
+        return in_use;
+    }
+
+    st->f_bsize  = _config.block_size;
+    st->f_frsize = _config.block_size;
+    st->f_blocks = _config.block_count;
+    st->f_bfree  = _config.block_count - in_use;
+    st->f_bavail = _config.block_count - in_use;
+    st->f_namemax = LFS_NAME_MAX;
+    return 0;
+}
+
+////// File operations //////
+int LittleFS::file_open(lfs_file_t *file, const char *path, int flags)
+{
+    int err = lfs_file_open(&_lfs, file, path, flags);
+    return err;
+}
+
+int LittleFS::file_close(lfs_file_t *file)
+{
+    int err = lfs_file_close(&_lfs, file);
+    return (err);
+}
+
+ssize_t LittleFS::file_read(lfs_file_t *file, void *buffer, size_t len)
+{
+    lfs_ssize_t res = lfs_file_read(&_lfs, file, buffer, len);
+    return (res);
+}
+
+ssize_t LittleFS::file_write(lfs_file_t *file, const void *buffer, size_t len)
+{
+    lfs_ssize_t res = lfs_file_write(&_lfs, file, buffer, len);
+    return (res);
+}
+
+int LittleFS::file_sync(lfs_file_t *file)
+{
+    int err = lfs_file_sync(&_lfs, file);
+    return (err);
+}
+
+off_t LittleFS::file_seek(lfs_file_t *file, off_t offset, int whence)
+{
+    off_t res = lfs_file_seek(&_lfs, file, offset, whence);
+    return (res);
+}
+
+off_t LittleFS::file_tell(lfs_file_t *file)
+{
+    off_t res = lfs_file_tell(&_lfs, file);
+    return (res);
+}
+
+off_t LittleFS::file_size(lfs_file_t *file)
+{
+    off_t res = lfs_file_size(&_lfs, file);
+    return (res);
+}
+
+int LittleFS::file_truncate(lfs_file_t *file, off_t length)
+{
+    int err = lfs_file_truncate(&_lfs, file, length);
+    return (err);
+}
+
+
+////// Dir operations //////
+int LittleFS::dir_open(lfs_dir_t *dir, const char *path)
+{
+    int err = lfs_dir_open(&_lfs, dir, path);
+    return (err);
+}
+
+int LittleFS::dir_close(lfs_dir_t *dir)
+{
+    int err = lfs_dir_close(&_lfs, dir);
+    return (err);
+}
+
+ssize_t LittleFS::dir_read(lfs_dir_t *dir, lfs_info *info)
+{
+    int res = lfs_dir_read(&_lfs, dir, info);
+    return (res);
+}
+
+void LittleFS::dir_seek(lfs_dir_t *dir, off_t offset)
+{
+    lfs_dir_seek(&_lfs, dir, offset);
+}
+
+off_t LittleFS::dir_tell(lfs_dir_t *dir)
+{
+    lfs_soff_t res = lfs_dir_tell(&_lfs, dir);
+    return (res);
+}
+
+void LittleFS::dir_rewind(lfs_dir_t *dir)
+{
+    lfs_dir_rewind(&_lfs, dir);
 }
--- LittleFS.h
+++ LittleFS.h
@@ -2,7 +2,8 @@
  * LittleFS.h
  *
  *  Created on: 11 Jun 2020
- *      Author: Casper
+ *  Heavily based on: https://github.com/ARMmbed/mbed-os/blob/master/features/storage/filesystem/littlefsv2/LittleFileSystem2.h
+ *
  */
 
 #ifndef LITTLEFS_LITTLEFS_H_
@@ -13,8 +14,25 @@
 
 #define LFS_READ_SIZE   512
 #define LFS_PROG_SIZE   512
-#define LFS_BLOCK_SIZE  64
+#define LFS_BLOCK_SIZE  512
+#define LFS_CACHE_SIZE  1       //in blocks
 #define LFS_LOOKAHEAD   64
+#define LFS_BLOCKCYCLES 1024
+
+typedef signed   int  ssize_t;  ///< Signed size type, usually encodes negative errors
+typedef unsigned int  size_t;
+typedef signed   long off_t;    ///< Offset in a data stream
+typedef unsigned long long fsblkcnt_t;  ///< Count of file system blocks
+
+typedef struct statvfs {
+     unsigned long  f_bsize;    ///< Filesystem block size
+     unsigned long  f_frsize;   ///< Fragment size (block size)
+     fsblkcnt_t     f_blocks;   ///< Number of blocks
+     fsblkcnt_t     f_bfree;    ///< Number of free blocks
+     fsblkcnt_t     f_bavail;   ///< Number of free blocks for unprivileged users
+     unsigned long  f_fsid;     ///< Filesystem ID
+     unsigned long  f_namemax;  ///< Maximum filename length
+ } statvfs_t;
 
 class LittleFS {
 public:
@@ -24,24 +42,85 @@ public:
                           lfs_size_t block_size = LFS_BLOCK_SIZE,
                           lfs_size_t lookahead = LFS_LOOKAHEAD);
     ~LittleFS();
-
-    int mount(SDCard *bd);
-
     int format(lfs_size_t read_size = LFS_READ_SIZE,
                           lfs_size_t prog_size = LFS_PROG_SIZE,
                           lfs_size_t block_size = LFS_BLOCK_SIZE,
                           lfs_size_t lookahead = LFS_LOOKAHEAD);
 
+    int mount(SDCard *bd);
+    int unmount();
+
+    //Remove a file from the file system.
+    int remove(const char *path);
+
+    // Rename a file in the file system.
+    int rename(const char *path, const char *newpath);
+
+    // Store information about the file in a stat structure
+    int stat(const char *path, lfs_info *st);
+
+    // Create a directory in the file system.
+    int mkdir(const char *path);
+
+    // Store information about the mounted file system in a statvfs structure.
+    int statvfs(const char *path, struct statvfs *buf);
+
+    // Open a file on the file system.
+    int file_open(lfs_file_t *file, const char *path, int flags);
+
+    // Close a file
+    int file_close(lfs_file_t *file);
+
+    // Read the contents of a file into a buffer
+    ssize_t file_read(lfs_file_t *file, void *buffer, size_t size);
+
+    // Write the contents of a buffer to a file
+    ssize_t file_write(lfs_file_t *file, const void *buffer, size_t size);
+
+    // Flush any buffers associated with the file
+    int file_sync(lfs_file_t *file);
+
+    // Move the file position to a given offset from a given location
+    off_t file_seek(lfs_file_t *file, off_t offset, int whence);
+
+    // Get the file position of the file
+    off_t file_tell(lfs_file_t *file);
+
+    // Get the size of the file
+    off_t file_size(lfs_file_t *file);
+
+    // Truncate or extend a file.
+    int file_truncate(lfs_file_t *file, off_t length);
+
+    // Open a directory on the file system.
+    int dir_open(lfs_dir_t *dir, const char *path);
+
+    // Close a directory
+    int dir_close(lfs_dir_t *dir);
+
+    // Read the next directory entry
+    ssize_t dir_read(lfs_dir_t *dir, lfs_info *ent);
+
+    // Set the current position of the directory
+    void dir_seek(lfs_dir_t *dir, off_t offset);
+
+    // Get the current position of the directory
+    off_t dir_tell(lfs_dir_t *dir);
+
+    // Rewind the current position to the beginning of the directory
+    void dir_rewind(lfs_dir_t *dir);
+
+    lfs_t _lfs; // The actual file system
 
 private:
-    lfs_t _lfs; // The actual file system
+
     struct lfs_config _config;
     SDCard *_bd; // The block device
 
     uint8_t readBuf[LFS_READ_SIZE];
     uint8_t progBuf[LFS_PROG_SIZE];
-    uint8_t lookaheadBuf[LFS_LOOKAHEAD];
-    lfs_file_t fileBuf;
+    uint8_t lkahBuf[LFS_LOOKAHEAD];
+    uint8_t cachBuf[LFS_BLOCK_SIZE * LFS_CACHE_SIZE];
 
     // default parameters
     const lfs_size_t _read_size;
